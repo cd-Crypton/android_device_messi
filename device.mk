f7429a4f271e727e226cfa2a845a5cf2e3c7423e
Carlo Dee <carlodee.official@proton.me>
2023-04-13 21:27:34 +0800

    messi/mapper: add graphics.napper.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- device.mk
+++ device.mk
@@ -146,6 +146,8 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
