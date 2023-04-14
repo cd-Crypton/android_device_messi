2505aa819305357eeaa910871c5044bb32aa5a45
Carlo Dee <carlodee.official@proton.me>
2023-04-15 00:56:01 +0800

    messi/update: added screen density and switch to dotpros.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- device.mk
+++ device.mk
@@ -183,7 +183,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
 # Dynamic Partitions
-PRODUCT_BUILD_SUPER_PARTITION := false
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
